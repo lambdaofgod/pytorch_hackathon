45a7cb755c079445e1a39e8735d2fa71f0c31f4c
Jakub Bartczuk <bartczukkuba@gmail.com>
2020-08-12 13:07:49 +0200

    computing zero-shot classification in app and caching

--- streamlit/rss_app.py
+++ streamlit/rss_app.py
@@ -1,21 +1,27 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import tqdm
-from haystack.database.elasticsearch import ElasticsearchDocumentStore
-from haystack.retriever.sparse import ElasticsearchRetriever
-import elasticsearch
+import os
+from operator import itemgetter
+
 from pytorch_hackathon import zero_shot_learning
 import ktrain
 import seaborn as sns
+from utils import streamlit_tqdm
+
+st.title('Zero-shot RSS feed article classifier')
 
 cm = sns.light_palette("green", as_cmap=True)
-topic_strings = zero_shot_learning.topic_strings.copy()
+topic_strings = list(pd.read_table('data/topics.txt', header=None).iloc[:,0].values)
+rss_feed_urls = list(pd.read_table('data/feeds.txt', header=None).iloc[:,0].values)
 rss_feed_urls = zero_shot_learning.rss_feed_urls.copy()
 
-#zsl_clf = ktrain.text.ZeroShotClassifier(device='cuda')
+
+model_device = st.selectbox("Model device", ["cpu", "cuda"], index=1)
 
 
-@st.cache
+@st.cache(allow_output_mutation=True)
 def get_feed_df():
     return zero_shot_learning.get_feed_df(rss_feed_urls)
 
@@ -23,19 +29,29 @@ def get_feed_df():
 feed_df = get_feed_df()
 
 
-def get_displayed_df(zsl_clf):
-    results_df = zero_shot_learning.get_zero_shot_classification_results_df(
-        zsl_clf,
-        feed_df['summary'],
-        topic_strings
-    )
-    return feed_df[['title', 'summary']].join(results_df)
+def get_displayed_df():
+    results_csv_path = 'data/zsl_feed_results.csv'
+    cached_result_exists = os.path.exists(results_csv_path)
+    if cached_result_exists:
+        results_df = pd.read_csv(results_csv_path)
+    else:
+        with st.spinner('No precomputed topics found, running zero-shot learning'):
+            zsl_clf = ktrain.text.ZeroShotClassifier(device=model_device)
+            results_df = zero_shot_learning.get_zero_shot_classification_results_df(
+                zsl_clf,
+                feed_df['text'],
+                topic_strings,
+                progbar_wrapper=streamlit_tqdm
+            )
+            results_df.to_csv(results_csv_path, index=False)
+    return feed_df[['title', 'text']].join(results_df)
 
-st.markdown('# Zero-shot RSS feed article classifier')
 
-display_df = pd.read_csv('feed_topics.csv', index_col='index').reset_index(drop=True)#get_displayed_df(zsl_clf)
-display_df['summary'] = display_df['summary'].apply(lambda s: s[:1000])
-topics = st.multiselect('Choose topics', topic_strings)
+selected_df = get_displayed_df().reset_index(drop=True)
+selected_df['text'] = selected_df['text'].apply(lambda s: s[:1000])
+topics = st.multiselect('Choose topics', topic_strings, default=[topic_strings[0]])
+sort_by = st.selectbox("Sort by", topics)
+display_df = selected_df[selected_df[topics].min(axis=1) > 0.5].sort_values(sort_by, ascending=False)
 
 st.markdown('## Articles on {}'.format(', '.join(topics)))
 
---
+++ streamlit/utils.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+class streamlit_tqdm:
+    def __init__(self, iterable, title=None):
+        if title:
+            st.write(title)
+        self.prog_bar = st.progress(0)
+        self.iterable = iterable
+        self.length = len(iterable)
+        self.i = 0
+
+    def __iter__(self):
+        for obj in self.iterable:
+            yield obj
+            self.i += 1
+            current_prog = self.i / self.length
+            self.prog_bar.progress(current_prog)
